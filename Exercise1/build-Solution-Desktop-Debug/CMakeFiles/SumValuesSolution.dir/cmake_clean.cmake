62a92577c70602f9d986b4da4235fbff6a231488
Daniele Semeraro <s284238@studenti.polito.it>
2024-03-18 12:02:58 +0100

    forse è la volta buona

---
+++ Exercise1/build-Solution-Desktop-Debug/CMakeFiles/SumValuesSolution.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SumValuesSolution.dir/main.cpp.obj"
+  "CMakeFiles/SumValuesSolution.dir/main.cpp.obj.d"
+  "SumValuesSolution.exe"
+  "SumValuesSolution.exe.manifest"
+  "SumValuesSolution.pdb"
+  "libSumValuesSolution.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SumValuesSolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
